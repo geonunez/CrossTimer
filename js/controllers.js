d305af5f3516311c23a6b7a57689787abb836857
geonunez <geonunez@gmail.com>
2014-02-02 23:33:55 -0300

    Added new beeps and fixed some issues

--- js/controllers.js
+++ js/controllers.js
@@ -5,13 +5,14 @@ app.controller('timerController',function($scope,$timeout){
     /** Attributes **/
 
     $scope.waitFor = 10; //Seconds
-    $scope.animate = false; //Indicates if the timer has to blink
-    $scope.beep = false; //Indicates if the timer has to beep
-    $scope.maxMinutes = 0; //Indicates the max minutes when the timer is upping
-    $scope.maxSeconds = 0; //Indicates the max seconds when the timer is upping
+    $scope.animate = false; //Indicate if the timer has to blink
+    $scope.beep = false; //Indicate if the timer has to beep
+    $scope.maxMinutes = 0; //Indicate the max minutes when the timer is upping
+    $scope.maxSeconds = 0; //Indicate the max seconds when the timer is upping
     $scope.now;
     $scope.idTimerInteval; //Timer Interval's id
     $scope.idWaitForInteval; //Timer Interval's id
+    $scope.isRunnig = false; //Indicate if the timer is running
     
 
     $scope.oMinutes = new Array();
@@ -23,30 +24,31 @@ app.controller('timerController',function($scope,$timeout){
 
     /** Public methods **/
 
-    $scope.start = function(){
-        
-        resetTimer();
-
-        var waitFor = parseInt($scope.waitFor,10);
-        
-        //Interval for the count down before start
-        $scope.idWaitForInteval = setInterval(function(){
-            countdownBeforeStart(waitFor);
-            waitFor -= 1;
-        },1000);
-
-        $timeout(function(){
-            startTimer();
-        }, (waitFor + 1) * 1000);
+    $scope.start = function(){  
+        if (!$scope.isRunnig){
+            resetTimer();
+            var waitFor = parseInt($scope.waitFor,10);
+            //Interval for the countdown before start
+            $scope.idWaitForInteval = setInterval(function(){
+                countdownBeforeStart(waitFor);
+                waitFor -= 1;
+            },1000);
+            //Wait for the countdown before start
+            $timeout(function(){
+                startTimer();
+            }, (waitFor + 1) * 1000);
+        }
     };
 
     $scope.stop = function(){
-        $scope.animate = true;
-        clearInterval($scope.idTimerInteval);
-        
-        if ($scope.beep){
-            beep();
-            $scope.beep = false;
+        if ($scope.isRunnig){
+            $scope.animate = true;
+            clearInterval($scope.idTimerInteval);
+            if ($scope.beep){
+                beepDouble();
+                $scope.beep = false;
+            }
+            $scope.isRunnig = false;
         }
     };
 
@@ -57,6 +59,7 @@ app.controller('timerController',function($scope,$timeout){
     /** Private methods **/
 
     function startTimer(){
+        $scope.isRunnig = true;
         //Up Timer
         if ($scope.program === $scope.getUpProgram()){
             var begin = new Date();
@@ -81,6 +84,18 @@ app.controller('timerController',function($scope,$timeout){
             audio.play();
     }
 
+    function beepBig(){
+        var audio = document.getElementById("beepBig");
+            audio.load();
+            audio.play();
+    }
+
+    function beepDouble(){
+        var audio = document.getElementById("beepDouble");
+            audio.load();
+            audio.play();
+    }
+
     function countdownBeforeStart(waitFor){
         //Shows the counter
         $scope.$apply(function(){
@@ -92,6 +107,7 @@ app.controller('timerController',function($scope,$timeout){
             beep();
         //Restore selected program and clear the inteval
         else if (waitFor === 0){
+            beepBig();
             $scope.$apply(function(){
                 $scope.selectedProgram = $scope.programNames[$scope.program];
             });
@@ -142,41 +158,40 @@ app.directive("clock",function(){
             $(element).clearQueue();
             $(element).fadeIn(500);
 
-            /*scope.hours = scope.now.getUTCHours() < 10 ? 
-            "0" + scope.now.getUTCHours() : 
-            scope.now.getUTCHours();*/
+            if (typeof $scope.now == "object"){
 
-            $scope.minutes = $scope.now.getUTCMinutes() < 10 ?
-            "0" + $scope.now.getUTCMinutes() :
-            $scope.now.getUTCMinutes();
+                $scope.minutes = $scope.now.getUTCMinutes() < 10 ?
+                "0" + $scope.now.getUTCMinutes() :
+                $scope.now.getUTCMinutes();
 
-            $scope.seconds = $scope.now.getUTCSeconds() < 10 ?
-            "0" + $scope.now.getUTCSeconds() :
-            $scope.now.getUTCSeconds();
+                $scope.seconds = $scope.now.getUTCSeconds() < 10 ?
+                "0" + $scope.now.getUTCSeconds() :
+                $scope.now.getUTCSeconds();
 
-            $scope.milliseconds = $scope.now.getUTCMilliseconds().toString().substr(0,2);
+                $scope.milliseconds = $scope.now.getUTCMilliseconds().toString().substr(0,2);
 
-            //Uptime and minutes and seconds are equals to max values
-            minutes = parseInt($scope.minutes,10);
-            seconds = parseInt($scope.seconds,10);
+                //Uptime and minutes and seconds are equals to max values
+                minutes = parseInt($scope.minutes,10);
+                seconds = parseInt($scope.seconds,10);
 
-            if ((minutes === 0  && seconds > 0) || minutes > 0){
+                if ((minutes === 0  && seconds > 0) || minutes > 0){
 
-                maxMinutes = parseInt($scope.maxMinutes,10);
-                maxSeconds = parseInt($scope.maxSeconds,10);
+                    maxMinutes = parseInt($scope.maxMinutes,10);
+                    maxSeconds = parseInt($scope.maxSeconds,10);
 
-               //Program 1 - Up
-               if ($scope.program === 1 &&
-                ((minutes === maxMinutes && seconds === maxSeconds) ||
-                (minutes === 99 && seconds === 59))){
-                    
-                    $scope.milliseconds = 
-                        (minutes === 99 && seconds === 59) ?
-                            "99":
-                            "00";
+                   //Program 1 - Up
+                   if ($scope.program === $scope.getUpProgram() &&
+                    ((minutes === maxMinutes && seconds === maxSeconds) ||
+                    (minutes === 99 && seconds === 59))){
+                        
+                        $scope.milliseconds = 
+                            (minutes === 99 && seconds === 59) ?
+                                "99":
+                                "00";
 
-                    $scope.beep = true;
-                    $scope.stop();
+                        $scope.beep = true;
+                        $scope.stop();
+                    }
                 }
             }
         });
